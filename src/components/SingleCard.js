48b2954e193e1ca72b9e6fed1915907c48d5f894
sharifa-assi <sharifaalassi2001@gmail.com>
2022-04-17 23:24:23 +0300

    some edits for timing and turns

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import "./SingleCard.css";
 
-function SingleCard({ card, handleChoice, flipped }) {
+function SingleCard({ card, handleChoice, flipped, disabled }) {
   const handleClick = () => {
-    handleChoice(card);
+    if (!disabled) {
+      handleChoice(card);
+    }
   };
 
   return (
